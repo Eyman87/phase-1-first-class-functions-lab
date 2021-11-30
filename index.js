1cafa104e2b098600ebbbb6698d27e07fa66edd5
Eyman Eghterafi <eghterafieyman@gmail.com>
2021-11-29 22:07:07 -0600

    commit tree 736369c8f979babd93e3c8c92bff7286b69635fd

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+// const returnFirstTwoDivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+function returnFirstTwoDrivers (drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers (drivers) {
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = function (num) {
+    return function (fare) {
+        return fare * num
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, x){
+  return x(drivers)
+}
+
+
+
